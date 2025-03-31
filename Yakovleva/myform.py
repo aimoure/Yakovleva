fff233fc4f9096e4ba2cc4ed856511d39c4f7c69
aimer <annaa5879@gmail.com>
2025-03-31 14:51:12 +0300

    Добавление записи вопросов в файл json

--- Yakovleva/myform.py
+++ Yakovleva/myform.py
@@ -2,15 +2,16 @@ from datetime import datetime
 from bottle import post, request, redirect
 import re
 import pdb
+import json
 
 @post('/home', method='post')
 def my_form():
-    questions = {}
+    question = {}
     mail = request.forms.get('ADRESS')
     name = request.forms.get('USERNAME')
     quest = request.forms.get('QUEST')  
-    questions[mail] = [name, quest]
-    pdb.set_trace()
+    question[mail] = [name, quest]
+    # pdb.set_trace()
 
     reg = r'^[a-zA-Z0-9]{2,}@[a-z]{2,}\.[a-z]{2,}$'
 
@@ -18,6 +19,9 @@ def my_form():
 
     if len(quest) < 1 or len(mail) < 1 or len(name) < 1:
         error += "All fields must be filled in. "
+
+    if len(quest) < 4 or quest.isdigit():
+        error += "The question is incorrect. "
 
     if (len(mail) < 8 or len(mail) > 255 or not re.match(reg, mail)) and len(mail) > 0: 
         error += "Mail entered incorrectly. "
@@ -27,5 +31,34 @@ def my_form():
 
     if len(error) > 0:
         return error
+    else:
+        try:
+            flag = 0
+            # Открываем файл для чтения
+            with open('static/json/questions.json', 'r') as file:
+                questions_from_file = json.load(file)
+                for q in question.keys(): # Проходимся по всем ключам из словаря (почтам)
+                    for q_f in questions_from_file.keys():
+                        if q == q_f: # Ищем совпадения
+                            flag = 1
+                            if questions_from_file[q][1] != question[q][1]:
+                                questions_from_file[q].append(question[q][1])
+                            else:
+                                return "Thanks, %s! Unfortunately, You've already asked this question. Access Date: %s" % (name, datetime.now().strftime("%d-%m-%Y"))
+            
+            if flag == 0:
+                questions_from_file.update(question)
 
-    return "Thanks, %s! The answer will be sent to the mail %s Access Date: %s" % (name, mail, datetime.now().strftime("%d-%m-%Y"))
+            # Открываем файл для записи
+            with open('static/json/questions.json', 'w') as file:
+                json.dump(questions_from_file, file, indent=4)
+        except FileNotFoundError:
+            print("Файл не найден")
+        except json.JSONDecodeError:
+            print("Ошибка декодирования JSON")
+        except PermissionError:
+            print("Недостаточно прав для доступа к файлу")
+        except Exception as e:
+            print(f"Произошла непредвиденная ошибка: {e}")
+
+        return "Thanks, %s! The answer will be sent to the mail %s. Access Date: %s" % (name, mail, datetime.now().strftime("%d-%m-%Y"))
